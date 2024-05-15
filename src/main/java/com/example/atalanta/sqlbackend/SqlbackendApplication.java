11953a5cd1d2a2e9d410f59f5a6872c51e68c024
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-05-14 21:13:05 -0400

    first commit

---
+++ src/main/java/com/example/atalanta/sqlbackend/SqlbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.atalanta.sqlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atalanta/sqlbackend/SqlbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atalanta.sqlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
